e092fa07c623a9c79857cb11102901a059d13d08
admhome <admhome@ya.ru>
2018-03-22 18:36:08 +0300

    Add separated output for web and console modes.

--- SimplestLogger.php
+++ SimplestLogger.php
@@ -5,17 +5,30 @@
 */
 class SimplestLogger
 {
+	protected $isCLI;
+
 	public function __construct()
 	{
+		$this->isCLI = (php_sapi_name() === 'cli');
+
 		return true;
 	}
 
 	public function log($data, $needDie = true) {
-		echo '<pre>' . var_export($data, true) . '</pre>';
+		if ($this->isCLI) {
+			echo '[DEBUG] Data: ' . var_export($data, true) . PHP_EOL;
+		} else {
+			echo '<pre>' . var_export($data, true) . '</pre>';
+		}
 
 		if ($needDie) {
 			$data = debug_backtrace();
-			die('<pre>Die in "' . $data[0]['file'] . '" on line: ' . $data[0]['line'] . '</pre>');
+
+			if ($this->isCLI) {
+				die('[DIE] File "' . $data[0]['file'] . '", line: ' . $data[0]['line'] . PHP_EOL);
+			} else {
+				die('<pre>Die in "' . $data[0]['file'] . '" on line: ' . $data[0]['line'] . '</pre>');
+			}
 		}
 
 		return true;
